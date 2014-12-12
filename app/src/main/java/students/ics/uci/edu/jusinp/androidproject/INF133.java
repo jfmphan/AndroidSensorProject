a20228bd3786e1e6c337cea7ab6e7b54a1e45e06
Justin Phan <jusinp@uci.edu>
2014-12-11 16:01:56 -0800

    Acceleration sensors have been added and it's working with sound. Two sounds do no work though.

--- app/src/main/java/students/ics/uci/edu/jusinp/androidproject/INF133.java
+++ app/src/main/java/students/ics/uci/edu/jusinp/androidproject/INF133.java
@@ -1,25 +1,43 @@
 package students.ics.uci.edu.jusinp.androidproject;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
 
-public class INF133 extends ActionBarActivity {
 
-    private SensorManager mSensorManager;
-    private SensorEventListener mOrientation;
+public class INF133 extends ActionBarActivity{
+
+    private MediaPlayer mediaPlayer;
+    private AssetFileDescriptor afd1;
+    private AssetFileDescriptor afd2;
+    private AssetFileDescriptor afd3;
+    private AssetFileDescriptor afd4;
+    private AssetFileDescriptor afd5;
+    private AssetFileDescriptor afd6;
+    private SensorManager sensorManager;
+    private SensorEventListener acc;
     private TextView textField01;
-    private float azimuthAngle;
-    private float pitchAngle;
-    private float rollAngle;
+
+
+    private double[] x = new double[2];
+    private double[] y = new double[2];
+    private double[] z = new double[2];
+    private double dX;
+    private double dY;
+    private double dZ;
 
     private void updateUI()
     {
@@ -27,23 +45,79 @@ public class INF133 extends ActionBarActivity {
 
             @Override
             public void run(){
-                textField01.setText(" " + pitchAngle);
+                textField01.setText("x : " + dX + " y: " + dY + " z: " + dZ);
             }
         });
 
     }
 
+    private double greatest(double x, double y, double z)
+    {
+        double greatest = 0;
+
+        double dx = Math.abs(x);
+        double dy = Math.abs(y);
+        double dz = Math.abs(z);
+
+        if(dx > dy)
+        {
+            greatest = x;
+        }
+        else
+        {
+            greatest = y;
+        }
+
+        if(greatest < dz)
+        {
+            greatest = z;
+        }
+
+        return greatest;
+    }
+
+
+    synchronized  void playAudio(AssetFileDescriptor afd){
+        if(mediaPlayer.isPlaying())
+        {
+            return;
+        }
+        else
+        {
+            try{
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+            }
+            catch (IllegalArgumentException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IllegalStateException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
     @Override
     protected void onResume(){
         super.onResume();
-        mSensorManager.registerListener(mOrientation, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(acc, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
         // register the listener
     }
 
     @Override
     protected void onStop(){
         // unregister the listener
-
+        sensorManager.unregisterListener(acc);
         super.onStop();
     }
 
@@ -52,28 +126,86 @@ public class INF133 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inf133);
-        textField01 = (TextView) findViewById(R.id.textField01);
+        textField01 = (TextView) findViewById(R.id.textView);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mOrientation = new SensorEventListener(){
 
+        z[1] = 0;
+        y[1] = 0;
+        x[1] = 0;
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        acc = new SensorEventListener() {
             @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy)
-            {
+            public void onSensorChanged(SensorEvent event) {
 
-            }
 
-            @Override
-            public void onSensorChanged(SensorEvent event){
+                z[0] = z[1];
+                y[0] = y[1];
+                x[0] = x[1];
 
-                azimuthAngle = event.values[0];
-                pitchAngle = event.values[1];
-                rollAngle = event.values[2];
+                z[1] = event.values[0];
+                y[1] = event.values[1];
+                x[1] = event.values[2];
+
+                dX = x[1] - x[0];
+                dY = y[1] - y[0];
+                dZ = z[1] - z[0];
                 updateUI();
+
+                double n = greatest(dX, dY, dZ);
+
+                if(dX == n)
+                {
+                    if(dX > 1){
+                        playAudio(afd1);
+                    }
+                    else if(dX < -1){
+                        playAudio(afd2);
+                    }
+                }
+                else if(dY == n)
+                {
+                    if(dY > 1){
+                        playAudio(afd3);
+                    }
+                    else if(dY < - 1){
+                        playAudio(afd4);
+                    }
+                }
+
+                else if(dZ == n)
+                {
+                    if(dZ > 1){
+                        playAudio(afd5);
+                    }
+                    else if(dZ < -1){
+                        playAudio(afd6);
+                    }
+                }
+
             }
 
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
         };
+
+        mediaPlayer = new MediaPlayer();
+        afd1 = getApplicationContext().getResources().openRawResourceFd(R.raw.classic);
+        afd2 = getApplicationContext().getResources().openRawResourceFd(R.raw.alpha);
+        afd3 = getApplicationContext().getResources().openRawResourceFd(R.raw.beeps);
+        afd4 = getApplicationContext().getResources().openRawResourceFd(R.raw.bell);
+        afd5 = getApplicationContext().getResources().openRawResourceFd(R.raw.bubble);
+        afd6 = getApplicationContext().getResources().openRawResourceFd(R.raw.instance);
+
+
     }
+
+
+
+
+
 
 
     @Override
